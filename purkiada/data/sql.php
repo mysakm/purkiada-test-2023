5202e1e1c143aa9bd4a98d603cac204dbec11910
Michal Myška <47862818+mysakm@users.noreply.github.com>
2024-04-16 22:28:53 +0200

    Create sql.php

---
+++ purkiada/data/sql.php
@@ -0,0 +1,12 @@
+<?php
+$host = ""; // adresa zařízení, které má běžící databázi.
+$user = ""; // jméno uživatele, který bude provádět běžné změny (nízká úroveň přístupu)
+$pass = ""; // heslo uživatele z minulého řádku
+$db = ""; // databáze (skript .sql automaticky vytvoří databázi pojmenovanou soutez-db)
+$anothauser = ""; // jméno uživatele, který bude provádět administrátorské změny (vysoká úroveň přístupu)
+$anothapass = ""; // heslo uživatele z minulého řádku
+$mailserver = ""; // adresa zařízení, které hostuje emailový server (potřeba na posílání emailů)
+$emailuser = ""; // email uživatele, který bude užit na automatické odesílání emailů
+$emailpass = ""; // heslo uživatele z minulého řádku
+$smtpport = ; // port, který zařízení emailového serveru využívá k přijímání emailů
+?>
